c157693b9692f5af092b1886e4209805fa276701
Selim Ozcan <65914446+selimozcann@users.noreply.github.com>
2021-03-12 18:12:48 +0300

    Knife Hit Clone

---
+++ Knife Hit Games/Assets/Scripts/BoardController/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeedScriptableData;
+
+namespace BoardControllerSystem
+{
+    public class BoardController : MonoBehaviour
+    {
+        [SerializeField] private SpeedScriptable _speedScriptable;
+        private void Update()
+        {
+            transform.Rotate(Vector3.back * _speedScriptable.BoardRotateSpeedProperty * Time.deltaTime );
+        }
+    }
+}
+
---
+++ Knife Hit Games/Assets/Scripts/ScriptableObject/SpeedScriptable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpeedScriptableData
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/SpeedScriptable")]
+    public class SpeedScriptable : ScriptableObject
+    {
+        [Header("BoardRotateSpeed")]
+        [SerializeField] private float _boardRotateSpeed;
+        public float BoardRotateSpeedProperty { get { return _boardRotateSpeed; } }
+
+        [Header("KnifeControllerSpeed")]
+        [SerializeField] private float _knifeControllerSpeed;
+        public float  KnifeSpeedProperty { get { return _knifeControllerSpeed; } }
+    }
+}
+
